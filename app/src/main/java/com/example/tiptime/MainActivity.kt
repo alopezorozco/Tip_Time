749603989607a7b3be83e80e6842cdbb43844db1
alopezorozco <alopezorozco@uaz.edu.mx>
2022-03-09 12:49:54 -0600

    Primer Commit de el segundo branch

--- app/src/main/java/com/example/tiptime/MainActivity.kt
+++ app/src/main/java/com/example/tiptime/MainActivity.kt
@@ -22,23 +22,25 @@ class MainActivity : AppCompatActivity() {
 
     private fun calculateTip() {
         val stringInTextField = binding.costOfService.text.toString()
-        val cost = stringInTextField.toDouble()
+
+        //cambio realizado en comparación a la versión 2
+        val cost = stringInTextField.toDoubleOrNull()
 
         if (cost==null) {
             binding.tipResult.text = ""
             return
         }
 
-        val selectedId = binding.tipOptions.checkedRadioButtonId
-        val tipPercentage = when (selectedId) {
+
+        val tipPercentage = when (binding.tipOptions.checkedRadioButtonId) {
             R.id.option_twenty_percent -> 0.20
             R.id.option_eighteen_percent -> 0.18
             else -> 0.15
         }
 
         var tip = tipPercentage * cost
-        val roundUp = binding.roundUpSwitch.isChecked
-        if (roundUp){
+
+        if (binding.roundUpSwitch.isChecked){
             tip = kotlin.math.ceil(tip)
         }
 
